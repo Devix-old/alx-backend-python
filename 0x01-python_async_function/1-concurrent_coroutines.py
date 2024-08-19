ecdac4ecd5dc027c971a2d6cfe5f58e84b073798
Devix-old <mohamed.elgharef@gmail.com>
2024-08-19 19:11:23 +0100

    mmm

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """
-Module to run multiple wait_random coroutines concurrently.
+Run multiple wait_random coroutines concurrently and measure execution time.
 """
+from typing import List
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Execute wait_random n times with max_delay and return the delays.
     """
-    return [await wait_random(max_delay) for _ in range(n)]
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return delays
