7f25968f7122b4f00192936a7e7b83993db3fe9b
Devix-old <mohamed.elgharef@gmail.com>
2024-08-19 18:47:24 +0100

    replacing typing.List with list

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,11 +2,10 @@
 """
 Module to run multiple wait_random coroutines concurrently.
 """
-from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """
     Execute wait_random n times with max_delay and return the delays.
     """
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Module for handling async tasks."""
-from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> list[float]:
     """
     Create n tasks with wait_random and return their results as a list.
     """
