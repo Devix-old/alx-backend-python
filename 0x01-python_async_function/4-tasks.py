1df3f73cc5d14d75d8e50e3273579223c5624323
Devix-old <mohamed.elgharef@gmail.com>
2024-08-20 09:35:19 +0100

    commit

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Module for handling async tasks."""
 task_wait_random = __import__('3-tasks').task_wait_random
+from typing import List
 
-
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Create n tasks with wait_random and return their results as a list.
     """
