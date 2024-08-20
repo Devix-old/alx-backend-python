1cb734aa7409ecebee7b4aae5e292c9f9cb62b2c
Devix-old <mohamed.elgharef@gmail.com>
2024-08-20 16:19:00 +0100

    0x02-python_async_comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ coroutine called async_generator that takes no arguments"""
 async_generator = __import__('0-async_generator').async_generator
+from typing import List
 
-
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """async_comprehension function"""
     return [i async for i in async_generator()]
